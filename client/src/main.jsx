a300a578562f9664b10ec01425bcd71440de6fed
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-12-14 03:17:04 -0800

    redux initialization

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,10 +6,13 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
+import{Provider} from "react-redux"
+import Store from "./Store.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <Provider store={Store}>
+          <App />
+      </Provider>
   </React.StrictMode>,
 )
