c790750aee65bec86266cc0e8d9b75b8926e2270
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-12-15 12:11:50 -0800

    for checking if user is not an educator for a course he is trying to get rolled in

--- server/middleware/rest.mid.js
+++ server/middleware/rest.mid.js
@@ -12,17 +12,24 @@ export const isEducator = (req,res,next)=>{
 export const isInstructor = (req,res,next)=>{
 
     const isInstructor = req.course && req.userId && req.course.instructor._id == req.userId
+    
     if(!isInstructor) return res.status(403).json({error:'You are Not authorized to add lesson to this course'})
-
     next()
 }
 
 export const isStudent = (req,res,next)=>{
-   const isStudent = req.userId && req.userId== req.enrollment.student._id
+   const isStudent = req.userId && req.userId== req.enrollment.Student._id
    if(!isStudent) return res.status(403).json({error:"User is not Enrolled"})
 
    next()
 }
+
+export const isMycourse =  (req,res,next)=>{
+    const isMycourse =  req.course && req.userId && req.course.instructor._id == req.userId
+    if(isMycourse ) return res.status(403).json({error:'You can not enroll in your own course'})
+    next()
+ }
+
 
 
 
