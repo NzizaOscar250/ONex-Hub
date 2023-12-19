4fc966b548154359430d299abae60ec177fadc21
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-12-19 12:46:33 -0800

    added

--- server/index.js
+++ server/index.js
@@ -7,27 +7,33 @@ import authroutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import courseRoutes from "./routes/course.routes.js"
 import enrollRoutes from "./routes/enroll.routes.js"
+import serverless from "serverless-http";
 
 dotenv.config();
 export const app = express()
-const PORT = process.env.PORT || 8000
 
 // const offlineUrl = "mongodb://127.0.0.1:27017/web_classroom"
 const onlineUrl = process.env.DB 
-
 app.use(bodyParser.json({limit:"30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit:'30mb',extended:true}))
 app.use(cors())
-app.use("/a",(req,res)=>res.send("Welcome oscar"))
 
-app.use("/auth/",authroutes)
-app.use("/user/",userRoutes)
-app.use("/courses/",courseRoutes)
-app.use("/enrollment/",enrollRoutes)
+app.use("/api/auth/",authroutes)
+app.use("/api/user/",userRoutes)
+app.use("/api/courses/",courseRoutes)
+app.use("/api/enrollment/",enrollRoutes)
+
 // app.use("/",(req,res)=>res.json({message:'working'}))
 
 mongoose.connect(onlineUrl).then(()=>{
-    app.listen(PORT,()=>console.log(`server started at: https:\\localhost:${PORT}`))
+    console.log("connected to mongo db")
 }).catch((e)=>console.log("error: ",e.message));
 
 
+const handler = serverless(app);
+
+// Export the handler for Netlify
+exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  return await handler(event, context);
+};
