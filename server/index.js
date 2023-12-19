d4e2d4d8c5773c5db1a37ab6f394bd8299c8ffe8
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-12-19 11:37:05 -0800

    deployment test on vercel

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import courseRoutes from "./routes/course.routes.js"
 import enrollRoutes from "./routes/enroll.routes.js"
 
 dotenv.config();
-const app = express()
+export const app = express()
 const PORT = process.env.PORT || 8000
 
 const offlineUrl = "mongodb://127.0.0.1:27017/web_classroom"
@@ -18,6 +18,7 @@ const onlineUrl = process.env.DB || null;
 app.use(bodyParser.json({limit:"30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit:'30mb',extended:true}))
 app.use(cors())
+app.use("/a",(req,res)=>res.send("Welcome oscar"))
 
 app.use("/auth/",authroutes)
 app.use("/user/",userRoutes)
@@ -27,6 +28,6 @@ app.use("/enrollment/",enrollRoutes)
 
 mongoose.connect(offlineUrl).then(()=>{
     app.listen(PORT,()=>console.log(`server started at: https:\\localhost:${PORT}`))
-}).catch((e)=>console.log("error: ",e.message))
+}).catch((e)=>console.log("error: ",e.message));
 
 
